0d4deeb9ece3069063121d854a16a5b66eb1ce8d
aakashr <aakash.rohilla@mavs.uta.edu>
2016-04-27 22:26:42 -0500

    Demo

--- public/js/wdm.js
+++ public/js/wdm.js
@@ -5,8 +5,8 @@ var t;
 $(document).ready(function() {
     $('#myBtn').on('click', function(event) {    	
         event.preventDefault();
-		$('#dynamictable').append('<table id="myTable" border="3" style="width:100%;background: black"></table>');
-        table = $('#dynamictable').children();
+		$('#tableData').append('<table id="myTable" border="3" style="width:100%;background: black"></table>');
+        table = $('#tableData').children();
     	table.append('<tr id=0 align="center"><th>Movie ID</th><th>Name of the Movie</th><th>Year of Release</th></tr>');
         callAjax(table)
         $('#myBtn').hide();
